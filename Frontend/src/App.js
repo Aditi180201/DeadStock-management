df27b63644ab4dccc78196f495a5e5fd6153e201
Aditi180201 <80048032+Aditi180201@users.noreply.github.com>
2022-05-17 16:36:54 +0530

    Add files via upload

---
+++ Frontend/src/App.js
@@ -0,0 +1,46 @@
+import './App.css'
+import Homepage from "./components/homepage/homepage"
+import Login from "./components/login/login"
+import Register from "./components/register/register"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { Redirect } from 'react-router-dom';
+
+function App() {
+
+  const [ user, setLoginUser] = useState({})
+
+  useEffect(() => {
+    setLoginUser(JSON.parse(localStorage.getItem("MyUser")))
+  }, [])
+
+  const updateUser = (user) => {
+    localStorage.setItem("MyUser", JSON.stringify(user))
+    setLoginUser(user)
+  }
+
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Homepage}>
+            {
+              user && user._id ? <Homepage updateUser={updateUser} /> : <Login updateUser={updateUser}/>
+            }
+          </Route>
+          <Route path="/login"  component={Login}>
+            <Login updateUser={updateUser}/> 
+          </Route>
+          <Route path="/register" component={Register}>
+          </Route>
+          <Route path="/404">
+          <h1>404 - Not found</h1>
+          </Route>
+          <Redirect to="/404" />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
